824b3c9a9458d4010d407f7180d6a881712cd154
Johan Larsson <johan_gavle1@hotmail.com>
2013-09-26 20:45:06 +0200

    Initial

---
+++ MeasurePlayer/FileControl.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace MeasurePlayer
+{
+    /// <summary>
+    /// Interaction logic for FileControl.xaml
+    /// </summary>
+    public partial class FileControl : UserControl
+    {
+        public FileControl()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty FilterProperty =
+            DependencyProperty.Register("Filter", typeof(string), typeof(FileControl), new PropertyMetadata(default(string)));
+        public string Filter
+        {
+            get { return (string)GetValue(FilterProperty); }
+            set { SetValue(FilterProperty, value); }
+        }
+
+        public static readonly DependencyProperty LabelTextProperty =
+            DependencyProperty.Register("LabelText", typeof(string), typeof(FileControl), new PropertyMetadata(default(string), (o, e) => ((FileControl)o).Label.Content = e.NewValue));
+        public string LabelText
+        {
+            get { return (string)GetValue(LabelTextProperty); }
+            set { SetValue(LabelTextProperty, value); }
+        }
+
+        public static readonly DependencyProperty PathProperty =
+            DependencyProperty.Register("Path", typeof(string), typeof(FileControl), new PropertyMetadata(default(string), (o, e) => ((FileControl)o).PathTb.Text = (string)e.NewValue));
+
+        public string Path
+        {
+            get { return (string)GetValue(PathProperty); }
+            set
+            {
+                SetValue(PathProperty, value);
+            }
+        }
+
+
+        private void BrowseClick(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new OpenFileDialog
+            {
+                Filter = Filter,
+                Multiselect = false
+            };
+            bool? showDialog = fileDialog.ShowDialog();
+            if (showDialog == true)
+            {
+                Path = fileDialog.FileName;
+                SetCurrentValue(PathProperty, fileDialog.FileName);
+            }
+
+        }
+
+        private void PathTb_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            Path = PathTb.Text;
+            //SetCurrentValue(PathProperty, PathTb.Text);
+        }
+
+    }
+}
---
+++ MeasurePlayer/RelayCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MeasurePlayer
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _action;
+        private readonly Predicate<object> _condition;
+        public RelayCommand(Action<object> action, Predicate<object> condition)
+        {
+            _action = action;
+            _condition = condition ?? (o => true);
+        }
+
+        public RelayCommand(Action<object> action)
+        {
+            _action = action;
+            _condition = (o) => true;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _condition(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _action(parameter);
+        }
+
+        /// <summary>
+        /// http://stackoverflow.com/a/2588145/1069200
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
